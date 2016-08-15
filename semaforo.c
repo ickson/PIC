f698f5819739c6a7e1e8c393d3cd8909c0e173ba
ickson <ickson94@hotmail.com>
2016-08-14 22:17:57 -0300

    Semaforo_v1.2
    Está funcionando corretamente (abertura e fechamento)

--- semaforo.c
+++ semaforo.c
@@ -12,6 +12,7 @@
 #define VERDE 3
 
 #include <xc.h>
+#include <pic16f628a.h>
  
 // BEGIN CONFIG
 #pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
@@ -24,27 +25,28 @@
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 //END CONFIG
 char estado = 0;
-
-void executaEstados(int porta, int tempo)
-{
-   PORTB = porta;  //Led Vermelho Ligado
-    __delay_ms(tempo); //Delay 5 segundos  
-}
+int tempo = 0;
 
 void configuraEstados()
 {
     switch (estado)
     {
         case 1:
-                executaEstados(0b00001000, 5000);  //Led Vermelho Ligado, Delay 5 segundos
+				//Led Vermelho Ligado, Delay 5 segundos
+                PORTA = 0b00001000;
+				__delay_ms(5000);  
                 estado = AMARELO;
                 break;
         case 2:
-                executaEstados(0b00010000, 2000); //Led Amarelo Ligado, Delay 2 segundos
+				//Led Amarelo Ligado, Delay 2 segundos
+                PORTA = 0b00010000;
+				__delay_ms(2000);
                 estado = VERDE;
                 break;
         case 3:
-                executaEstados(0b00100000, 5000); //Led Verde Ligado, Delay 5 segundos
+				//Led Verde Ligado, Delay 5 segundos
+                PORTA = 0b00100000;
+				__delay_ms(5000);
                 estado = VERMELHO;
                 break;
         
